daf4cecac594d1284d3b8afb219de9b7852a3d3b
Bryan Santiago <bryansatiago688@gmail.com>
2025-05-05 16:16:57 -0600

    Mi proyecto vue

---
+++ src/test/java/com/bryan/alumnos/AlumnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryan.alumnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
