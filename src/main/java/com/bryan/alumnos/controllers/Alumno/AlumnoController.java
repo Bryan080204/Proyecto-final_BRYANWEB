3c2e1ed58e947af2b9cf10ad7d685c782595a759
Bryan Santiago <bryansatiago688@gmail.com>
2025-05-08 22:04:05 -0600

    Actividad6-Creando metodo PUT en el controlador

--- src/main/java/com/bryan/alumnos/controllers/Alumno/AlumnoController.java
+++ src/main/java/com/bryan/alumnos/controllers/Alumno/AlumnoController.java
@@ -3,8 +3,11 @@ package com.bryan.alumnos.controllers.Alumno;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +35,24 @@ public class AlumnoController {
         return alumnoRepository.save(alumno);
  
 }
+
+@PutMapping ("/editar-alumnos/{id}")
+public ResponseEntity< Alumno> actualizarAlumno (@PathVariable long id, @RequestBody Alumno alumno){
+    return alumnoRepository.findById(id).map(alumnoExistente -> {
+ 
+alumnoExistente.setNombre(alumno.getNombre());
+
+alumnoExistente.setApellido(alumno.getApellido());
+
+alumnoExistente.setEmail(alumno.getEmail());
+alumnoExistente.setNumeroControl(alumno.getNumeroControl());
+alumnoExistente.setCarrera (alumno.getCarrera());
+alumnoExistente.setImagenURL(alumno.getImagenURL());
+Alumno actualizado = alumnoRepository.save(alumnoExistente);
+return ResponseEntity.ok (actualizado);
+}).orElse (ResponseEntity.notFound().build());
+ 
+
 }
-      
+
+}     
