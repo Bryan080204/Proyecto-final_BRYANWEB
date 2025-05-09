daf706706e1b1493c49f73da0d5fbe02f5c58ce4
Bryan Santiago <bryansatiago688@gmail.com>
2025-05-08 22:16:00 -0600

    Act7-Creando Metodo delete dentro del controlador

--- src/main/java/com/bryan/alumnos/controllers/Alumno/AlumnoController.java
+++ src/main/java/com/bryan/alumnos/controllers/Alumno/AlumnoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,4 +56,10 @@ return ResponseEntity.ok (actualizado);
 
 }
 
+@DeleteMapping("/eliminar-alumnos/{id}")
+public void eliminarAlumno (@PathVariable Long id) {
+
+alumnoRepository.deleteById(id);
+
 }     
+}    
